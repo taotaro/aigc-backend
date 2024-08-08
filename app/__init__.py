6ec73c3f8bd01f712a53560d19d935f3c58c7af9
kavyagb22 <kgbalaji@connect.ust.hk>
2024-08-08 17:41:36 +0800

    cors policy

--- app/__init__.py
+++ app/__init__.py
@@ -34,7 +34,7 @@ def create_app():
         await init_db(mongo_client)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        # allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
